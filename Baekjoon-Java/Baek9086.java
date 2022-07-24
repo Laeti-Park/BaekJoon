f3d9e6fcb1a73b6d6d68de27edc34383d34eab2a
Laeti-Park <creator98@naver.com>
2022-07-24 22:42:19 +0900

    [Add] Baek10807 : 개수 세기기

---
+++ Baekjoon-Java/Baek9086.java
@@ -0,0 +1,33 @@
+/* 
+ * 9086번 문제 : 문자열
+ * 
+ * 문제 : 문자열을 입력으로 주면 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 : 입력의 첫 줄에는 테스트 케이스의 개수 T(1 ≤ T ≤ 10)가 주어진다.
+ * 각 테스트 케이스는 한 줄에 하나의 문자열이 주어진다.
+ * 문자열은 알파벳 A~Z 대문자로 이루어지며 알파벳 사이에 공백은 없으며 문자열의 길이는 1000보다 작다.
+ * 
+ * 출력 : 각 테스트 케이스에 대해서 주어진 문자열의 첫 글자와 마지막 글자를 연속하여 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int case_num = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < case_num; i++) {
+            String str = reader.readLine();
+
+            if (str.length() == 1) {
+                System.out.println(str + "" +  str);
+            } else {
+                System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+            }
+        }
+    }
+}
