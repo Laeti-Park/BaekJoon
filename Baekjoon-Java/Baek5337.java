956399a3e3b9c51fea93f913ccd524d3060bdb0f
Laeti-Park <creator98@naver.com>
2022-08-06 23:41:16 +0900

    [Add] Baek4999, Baek5337

---
+++ Baekjoon-Java/Baek5337.java
@@ -0,0 +1,7 @@
+public class Baek5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
