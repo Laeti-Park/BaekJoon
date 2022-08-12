ee94435312f6e69992daaeacdc511bea799cfbc6
Laeti-Park <creator98@naver.com>
2022-08-13 00:00:32 +0900

    [Add] Baek15733, Baek17295, Baek24900

---
+++ Baekjoon-Java/Baek17295.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Baek17295 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Avengers: Endgame");
+    }
+}
