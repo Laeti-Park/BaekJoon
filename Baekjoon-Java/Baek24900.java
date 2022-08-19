58cd1500764df897a62f56d737f8c56579cd129c
Laeti-Park <creator98@naver.com>
2022-08-20 00:00:59 +0900

    [Add] Baek1264 : 모음의 개수

--- Baekjoon-Java/Baek24900.java
+++ Baekjoon-Java/Baek24900.java
@@ -1,5 +1,3 @@
-package Blossom.Database;
-
 public class Baek24900 {
     public static void main(String[] args) {
         System.out.println("                                                           :8DDDDDDDDDDDDDD$.                                           \n" +
