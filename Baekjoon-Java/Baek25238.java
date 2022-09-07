63775f66d7ffd6d08db19b80e9a03b30ee201606
Laeti-Park <creator98@naver.com>
2022-09-07 23:10:52 +0900

    [Add] Baek25238

---
+++ Baekjoon-Java/Baek25238.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek25238 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
+
+        if (a - (a / 100 * b) < 100) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
