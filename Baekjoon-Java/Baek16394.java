610db05258816c0bf40967035471501573cfbd72
Laeti-Park <creator98@naver.com>
2022-09-09 23:04:46 +0900

    [Update] Baek16394

--- Baekjoon-Java/Baek16394.java
+++ Baekjoon-Java/Baek16394.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Baek16394 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int year = scanner.nextInt();
-        System.out.println(year - 1946);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int year = scanner.nextInt();
+            System.out.println(year - 1946);
+        }
     }
 }
