e13cd6a18f28e6585e7141abc4725bc8953a7b23
Laeti-Park <creator98@naver.com>
2022-09-15 23:22:07 +0900

    [Add] 10797 : 구현

---
+++ Baekjoon-Java/Baek10797.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek10797 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));     
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			int car = Integer.parseInt(st.nextToken());
+			if(car == num) {
+				count++;
+			}
+		}
+		System.out.println(count);
+        
+    }
+}
