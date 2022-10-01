aa1f54d9fdd89d76bd932cccf9a183d8746aa1b4
Laeti-Park <creator98@naver.com>
2022-10-01 23:54:26 +0900

    [Update] Baek2522 : 구현

--- Baekjoon-Java/Baek2522.java
+++ Baekjoon-Java/Baek2522.java
@@ -9,19 +9,19 @@ public class Baek2522 {
         int num = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num-i-1; j++) {
+            for (int j = 0; j < num - i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int j = num; j > num-i-1; j--) {
+            for (int j = num; j > num - i - 1; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = num-1; i > 0; i--) {
-            for (int j = 0; j < num-i; j++) {
+        for (int i = num - 1; i > 0; i--) {
+            for (int j = 0; j < num - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = num; j > num-i; j--) {
+            for (int j = num; j > num - i; j--) {
                 System.out.print("*");
             }
             System.out.println();
