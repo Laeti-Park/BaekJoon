3fc4ca39849d2d568938d44d558d51d87db42a44
Laeti-Park <creator98@naver.com>
2022-10-09 18:25:34 +0900

    Update 8958 : 구현, 문자열

--- Baekjoon-Python/8958.py
+++ Baekjoon-Python/8958.py
@@ -5,8 +5,8 @@ for i in range(case):
     score = 0
     add = 0
     for j in range(len(ans)):
-        if(ans[j] == 'O'):
-            if(j != 0 and ans[j] == ans[j-1]):
+        if (ans[j] == 'O'):
+            if (j != 0 and ans[j] == ans[j-1]):
                 add = int(add + 1)
             score = int(score + add + 1)
         else:
