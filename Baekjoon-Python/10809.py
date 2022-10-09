36de5fdbfa4ef728bf8b4417cb78f49f73f336c0
Laeti-Park <creator98@naver.com>
2022-10-09 18:26:06 +0900

    Update 10809 : 구현, 문자열

--- Baekjoon-Python/10809.py
+++ Baekjoon-Python/10809.py
@@ -1,6 +1,6 @@
 lst = [-1 for i in range(26)]
 case = str(input())
 for i in range(len(case)):
-    if(lst[ord(case[i])-97] == -1):
+    if (lst[ord(case[i])-97] == -1):
         lst[ord(case[i])-97] = i
 print(' '.join(map(str, lst)))
