de1b50a14bca30f63ea2bcc28467b8b1fb52bede
Laeti-Park <creator98@naver.com>
2022-10-09 18:16:16 +0900

    Update 1065 : 브루트포스 알고리즘

--- Baekjoon-Python/1065.py
+++ Baekjoon-Python/1065.py
@@ -4,14 +4,14 @@ ans = 0
 for i in range(case):
     if (i+1 < 100):
         ans += 1
-    elif(i+1 == 1000):
+    elif (i+1 == 1000):
         break
     else:
         temp = i+1
         lst = list()
-        while(temp != 0):
-            lst.append(temp%10)
+        while (temp != 0):
+            lst.append(temp % 10)
             temp = int(temp / 10)
-        if(lst[2]-lst[1] == lst[1] - lst[0]):
-            ans+= 1
+        if (lst[2]-lst[1] == lst[1] - lst[0]):
+            ans += 1
 print(ans)
