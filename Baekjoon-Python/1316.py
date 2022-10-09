61c312611aa9ef6aedf843b09459204a7cb6f292
Laeti-Park <creator98@naver.com>
2022-10-09 18:17:58 +0900

    Update 1316 : 구현, 문자열

--- Baekjoon-Python/1316.py
+++ Baekjoon-Python/1316.py
@@ -4,19 +4,19 @@ count = 0
 for i in range(case):
     chk = True
     alphabet = [0 for i in range(26)]
-    word = str(input()) # 단어 입력
+    word = str(input())  # 단어 입력
 
     for j in range(len(word)):
-        if(j == 0):
+        if (j == 0):
             alphabet[ord(word[j]) - ord('a')] += 1
-        elif(j >= 1):
-            if(word[j-1] == word[j]):
+        elif (j >= 1):
+            if (word[j-1] == word[j]):
                 continue
             else:
                 alphabet[ord(word[j]) - ord('a')] += 1
     for k in range(len(alphabet)):
-        if(alphabet[k] > 1):
+        if (alphabet[k] > 1):
             chk = False
-    if(chk == True):
+    if (chk == True):
         count += 1
 print(count)
