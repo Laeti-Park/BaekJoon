5f38e9af7bec707e78e52b7837c958d9709424c7
Laeti-Park <creator98@naver.com>
2022-10-09 18:23:12 +0900

    Update 2908 : 수학, 구현

--- Baekjoon-Python/2908.py
+++ Baekjoon-Python/2908.py
@@ -1,7 +1,7 @@
 case = str(input())
 num = case.split(' ')
 num[0] = num[0][::-1]
-num[1] = num[1][::-1] # 문자열 뒤집기
+num[1] = num[1][::-1]  # 문자열 뒤집기
 
 if (int(num[0]) > int(num[1])):
     print(num[0])
