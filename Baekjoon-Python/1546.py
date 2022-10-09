b28dd3089de44b3413fe50621df593281e8070dc
Laeti-Park <creator98@naver.com>
2022-10-09 18:19:53 +0900

    Update 1546 : 수학, 사칙연산

--- Baekjoon-Python/1546.py
+++ Baekjoon-Python/1546.py
@@ -1,5 +1,5 @@
 case = int(input())
-num = list(map(int, input().split())) #공백으로 나누어 int형 리스트를 만든다.
+num = list(map(int, input().split()))  # 공백으로 나누어 int형 리스트를 만든다.
 max = int(num[0])
 for i in range(case):
     if max < num[i]:
