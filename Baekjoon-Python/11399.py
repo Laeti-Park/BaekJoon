be1a2a208079eadd84aca3f75bfcb136c16d776f
Laeti-Park <creator98@naver.com>
2022-11-06 23:06:29 +0900

    Add 11399 : 그리디 알고리즘, 정렬

---
+++ Baekjoon-Python/11399.py
@@ -0,0 +1,14 @@
+num = int(input())
+N = input().split(' ')
+N.sort()
+list = []
+sum = 0
+
+for i in range(len(N)):
+    if i == 0:
+        list.append(int(N[i]))
+    else:
+        list.append(int(list[i-1]) + int(N[i]))
+    sum += list[i]
+
+print(sum)
