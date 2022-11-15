bf0d94163570894b82b32031d5952aa1ff9dff60
Laeti-Park <creator98@naver.com>
2022-11-16 01:14:50 +0900

    Add 1929 : 수학, 정수론, 소수 판정, 에라토스테네스의 체

---
+++ Baekjoon-Python/1929.py
@@ -0,0 +1,23 @@
+import math
+import sys
+
+input = sys.stdin.readline
+
+def is_prime(parse_int):
+    if parse_int == 1:
+        return False
+
+    sqrt = int(math.sqrt(parse_int))
+
+    for i in range(2, sqrt+1):
+        if parse_int % i == 0:
+            return False
+
+    return True
+
+
+min_num, max_num = map(int, input().split())
+
+for i in range(min_num, max_num+1):
+    if is_prime(i) == True:
+        print(i)
