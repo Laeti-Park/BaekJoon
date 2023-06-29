b8b556fc72ca101ba0cb8e46e6f6e2e20f68d41d
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-06-30 01:27:31 +0900

    💯Solve(4344) : 평균은 넘겠지(수학, 사칙연산)

--- python/4344.py
+++ python/4344.py
@@ -1,3 +1,5 @@
+# 4344번 평균은 넘겠지
+
 case = int(input())
 
 for _ in range(case):
@@ -11,9 +13,10 @@ for _ in range(case):
         if (avg < test[j]):
             count += 1
 
+    # 사사오입 방식으로 변환
     roundOff = 0
     if len(str(count / test[0])) > 7:
-        if int(str(count / test[0])[7]) > 4:
+        if int(str(count / test[0])[6]) % 2 == 0:
             roundOff = 0.001
 
     print(f"{round(count / test[0] * 100, 3) + roundOff:.3f}%")
