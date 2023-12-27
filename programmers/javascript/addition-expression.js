f8a1aabb7a8acab39280e4374665b05eeaf19253
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-12-27 15:35:39 +0900

    💯Solve: 덧셈식 출력하기

---
+++ programmers/javascript/addition-expression.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181947
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+  input = line.split(' ').map(Number);
+  rl.close();
+}).on('close', () => {
+  console.log(`${input[0]} + ${input[1]} = ${input[0] + input[1]}`);
+});
