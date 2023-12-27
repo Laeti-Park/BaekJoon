6e08d949f1771396d585963b586976452b784e54
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-12-27 15:36:17 +0900

    💯Solve: 문자열 붙여서 출력하기

---
+++ programmers/javascript/concatenate-string-and-print.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181947
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+  input = line.split(' ');
+  rl.close();
+}).on('close', () => {
+  console.log(`${input[0]}${input[1]}`);
+});
