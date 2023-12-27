2a09136dd9a7ffc1d86c395c879351ba4feb6454
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-12-27 18:28:41 +0900

    💯Solve: 덧셈식 출력하기

--- programmers/javascript/printing-addition-equations.js
+++ programmers/javascript/printing-addition-equations.js
@@ -3,13 +3,13 @@
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
+rl.on('line', line => {
   input = line.split(' ').map(Number);
-}).on('close', function () {
+}).on('close', () => {
   console.log(`${input[0]} + ${input[1]} = ${input[0] + input[1]}`);
 });
