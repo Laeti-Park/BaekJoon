8147023ad24cb076148f7f62886f9b58125f99d4
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-12-27 18:29:00 +0900

    💯Solve: 문자열 섞기

--- programmers/javascript/shuffling-strings.js
+++ programmers/javascript/shuffling-strings.js
@@ -21,11 +21,11 @@ rl.on('line', (line) => {
   console.log(answer);
 });
 
-function solution(str1, str2) {
+const solution = (str1, str2) => {
   let answer = '';
   for (let i = 0; i < str1.length; i++) {
     answer += str1[i] + str2[i];
   }
 
   return answer;
-}
+};
