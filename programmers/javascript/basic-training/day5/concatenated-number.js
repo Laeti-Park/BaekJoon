3c9824f04d83ba1c810859168c58db1ebbdaeb87
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2024-01-01 17:20:10 +0900

    💯Solve: basic-training/day5

--- programmers/javascript/basic-training/day5/concatenated-number.js
+++ programmers/javascript/basic-training/day5/concatenated-number.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181929
+// https://school.programmers.co.kr/learn/courses/30/lessons/181928
 
 const readline = require('readline');
 const rl = readline.createInterface({
