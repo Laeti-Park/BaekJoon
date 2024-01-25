986b6dd37130e9b93857e9a878be111e92cb702c
Donghun Park <creator98@naver.com>
2024-01-26 00:58:23 +0900

    💯Solve: basic-training/day1

--- programmers/javascript/basic-training/day1/change-case.js
+++ programmers/javascript/basic-training/day1/change-case.js
@@ -9,17 +9,18 @@ let input = [];
 
 rl.on('line', line => {
   input = [line];
+  rl.close();
 }).on('close', () => {
   const str = input[0];
-  const small = 'a'.codePointAt();
-  const diff = 'a'.codePointAt() - 'A'.codePointAt();
+  const small = 'a'.codePointAt(0);
+  const diff = 'a'.codePointAt(0) - 'A'.codePointAt(0); // 32
   let result = '';
 
   for (let i = 0; i < str.length; i++) {
-    const char = str[i].codePointAt();
-    if (char < small) {
+    const char = str[i].codePointAt(0);
+    if (char < small) { // 대문자인 경우
       result += String.fromCodePoint(char + diff);
-    } else {
+    } else { // 소문자인 경우
       result += String.fromCodePoint(char - diff);
     }
   }
