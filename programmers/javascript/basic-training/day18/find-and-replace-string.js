675af836457437fc388f65f374526a35e1b0c0fb
Donghun Park <creator98@naver.com>
2024-03-18 01:15:44 +0900

    💯Solve: basic-training/day18

--- programmers/javascript/basic-training/day18/find-and-replace-string.js
+++ programmers/javascript/basic-training/day18/find-and-replace-string.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181872
+// https://school.programmers.co.kr/learn/courses/30/lessons/181864
 
 const readline = require('readline');
 const rl = readline.createInterface({
