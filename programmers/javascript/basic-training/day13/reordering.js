21456590998d0be07c1ea61a9aa1139a91b232f6
Donghun Park <creator98@naver.com>
2024-03-19 02:59:01 +0900

    💯Solve: basic-training/day13

--- programmers/javascript/basic-training/day13/reordering.js
+++ programmers/javascript/basic-training/day13/reordering.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181892
+// https://school.programmers.co.kr/learn/courses/30/lessons/181891
 
 const readline = require('readline');
 const rl = readline.createInterface({
