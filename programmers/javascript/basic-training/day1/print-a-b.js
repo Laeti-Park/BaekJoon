79a1cde7a58aa0b5171be98f9daf3d45d2d2d3fe
Donghun Park <creator98@naver.com>
2024-03-19 21:15:34 +0900

    💯Solve: basic-training/day1

--- programmers/javascript/basic-training/day1/print-a-b.js
+++ programmers/javascript/basic-training/day1/print-a-b.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181951?language=javascript
+// https://school.programmers.co.kr/learn/courses/30/lessons/181951
 
 const readline = require('readline');
 const rl = readline.createInterface({
