5fc45cfded2dca3786e0c490418e6a18324f78af
Donghun Park <creator98@naver.com>
2024-03-23 00:44:46 +0900

    💯Solve: basic-training/day16

--- programmers/javascript/basic-training/day16/convert-char-to-upper.js
+++ programmers/javascript/basic-training/day16/convert-char-to-upper.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181874
+// https://school.programmers.co.kr/learn/courses/30/lessons/181873
 
 const readline = require('readline');
 const rl = readline.createInterface({
